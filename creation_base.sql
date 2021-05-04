8e68e88c230567ccf2cca9286dbb8afdb578fb92
Maxime175 <67310230+Maxime175@users.noreply.github.com>
2021-05-04 12:15:21 +0200

    Add files via upload

---
+++ creation_base.sql
@@ -0,0 +1,33 @@
+/*   	Commandes de création de la base  */
+DROP TABLE IF EXISTS  Client;
+DROP TABLE IF EXISTS Historique_info;
+DROP TABLE IF EXISTS Historique_api;
+DROP TABLE IF EXISTS Information;
+
+CREATE TABLE Client(id_client INTEGER NOT NULL, 
+                         login VARCHAR (30) NOT NULL ,
+                         mdp VARCHAR (30) NOT NULL,
+                         PRIMARY KEY (id_client));
+ 
+CREATE TABLE Information (id_info   INTEGER NOT NULL,
+                          luminosite  INTEGER NOT NULL,
+                          niv_batt  INTEGER NOT NULL,
+		 	  pression  INTEGER NOT NULL,
+			  temperature  INTEGER NOT NULL,
+		          position_gps  INTEGER NOT NULL,
+                          PRIMARY KEY (id_info));
+
+CREATE TABLE Historique_info  (id_hist_info INTEGER NOT NULL,
+                       dateheure DATETIME NOT NULL,
+                       id_info INTEGER NOT NULL,
+                       PRIMARY KEY (id_hist_info),
+                       FOREIGN KEY (id_info) REFERENCES Information(id_info));
+
+CREATE TABLE Historique_api  (id_hist_api INTEGER NOT NULL,
+		    id_client INTEGER NOT NULL,
+                    dateheure    DATETIME NOT NULL,
+                    id_info    INTEGER NOT NULL,
+                    type_comm  VARCHAR(30) NOT NULL,
+                    PRIMARY KEY (id_hist_api),
+                    FOREIGN KEY (id_client) REFERENCES Client(id_client),
+		    FOREIGN KEY (id_info) REFERENCES Information(id_info));
