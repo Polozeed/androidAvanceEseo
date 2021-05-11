97bf555d4276bcaed288cf0eb34664fae6f3aa56
Maxime175 <maxime.pouplain@gmail.com>
2021-05-11 22:15:41 +0200

    Autorisation url

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -29,7 +29,7 @@ public class SpringBootDataJpaApplication {
 					.authorizeRequests()
 					//.antMatchers(HttpMethod.POST,"/client/connexion" ).permitAll()
 					.antMatchers(HttpMethod.POST, "/client/inscription","/client/connexion","/api/test/martin").permitAll()
-                    .antMatchers(HttpMethod.GET,"/api/test/martin","/client/liste","/client/{id}").permitAll()
+                    .antMatchers(HttpMethod.GET,"/api/test/martin","/client/liste","/client/{id}","/information/liste", "/information/{id}").permitAll()
 
                     .anyRequest().authenticated();
 		}
