c0c2d78b9d7057ff64302a0444004883fa32030c
Mcourier <martincourierdemere@gmail.com>
2021-05-11 16:24:48 +0200

    Correction BDD new version

--- src/main/java/com/bezkoder/spring/datajpa/config/test.sql
+++ src/main/java/com/bezkoder/spring/datajpa/config/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 10 mai 2021 à 13:54
+-- Généré le : mar. 11 mai 2021 à 16:24
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 7.4.14
 
@@ -55,7 +55,22 @@ INSERT INTO `client` (`id_client`, `login`, `mdp`, `token`) VALUES
 (37, 'm', 'ezz', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjAzMDI2ODUsImV4cCI6MTYyMDMwMzI4NX0.nlVMSo7s_ekYR9DbywWFX7QFDnlUYRnG8HpzOWvT1dxB_BdPxY1gBuxH5Mp9vGXu5yBdgrLnPdLu6nyb4npu8Q'),
 (38, 'm', 'ezz', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjAzMDM0ODksImV4cCI6MTYyMDMwNDA4OX0.b6dHt4ti4jrWL-R3BYVlzHny_6ErU11CSNlTx6uDjixh1o3Gps5z6AwWjbxf2hVid_c9-eV2lwCgf_khtTmlGg'),
 (39, 'm', 'ezz', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjAzMDM1ODksImV4cCI6MTYyMDMwNDE4OX0.OLVZ5sv7w6x_a0kOtWRuC8Q3G-MumnhuijrodIxXOYhlBtjIJjIVPGOaSVTi_g9WRPuTKIihJjSHh4RVnm7lHQ'),
-(40, 'maxime', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWF4aW1lIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYyMDYzNzQwMywiZXhwIjoxNjIwNjM4MDAzfQ.Ozvkc_J24MKPHC4DU0bSv2iw23ZuWWzKUmzNw3STiHpLXG2K9Eqs-hCfU1TLI195yWhEnttQs_07PNkkEP4LzQ');
+(40, 'maxime', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWF4aW1lIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYyMDY3NzA2NiwiZXhwIjoxNjIwNjc3NjY2fQ.-KoHPj--h6GlvmJ6oTbdp2vcJrolklqy_vnyp3DusjRQg3Fhd4qaNXoWxFo65syNnTFEx1HiGfnWyk4iTkxnfw'),
+(41, 'maxime', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWF4aW1lIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYyMDY3MTU0NywiZXhwIjoxNjIwNjcyMTQ3fQ.ABWS7Tsluzx1SValuZKVWWeu-O31Vv71ukQ6WOfWUl0OZLujRzd_MA5gKJy4qttH_cIhpX5GFLd9XeMI4H1mqw'),
+(42, 'mar', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWFyIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYyMDY3NzA4NSwiZXhwIjoxNjIwNjc3Njg1fQ.MDrXXVrt7DPD4JcZu7I4IXBBIUYXd7TDp5ADchqVdswWZUnSx4nNsx4ejjA-5DUeo86WBZleTGcLUFTZyjZblQ'),
+(43, 'mar2', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWFyMiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjA2NzcwOTcsImV4cCI6MTYyMDY3NzY5N30.qlit1Veg9T9hxZQIVYSohsnCO6jpVNQ_BxRfV9VzWE58LU4kq0N8YGkJLaPXZ71vpX15GmxIRvtgMRp05l2yuA'),
+(44, 'mar2', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWFyMiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjA2Nzc2OTQsImV4cCI6MTYyMDY3ODI5NH0.HIM9Qw2g2A0sudiJ95seVuw5onc38dhJfqEcELOAnWKU6BWUPnapX7QpIqA_-ymPz0nxIpTmFTIzNK_niYINOA'),
+(45, 'm', 'test', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjA2Nzc3MjgsImV4cCI6MTYyMDY3ODMyOH0.ZKoVKGaQi1RcWvyG5Vd2Hd6Z_CLAWU2j0c-fm7Y3W5n7R1uIqsOqf8anQA4Gkb2jVq9aVI63LddCqqQP1Aq8UQ'),
+(46, 'm', 'd', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjA2Nzc5ODUsImV4cCI6MTYyMDY3ODU4NX0.mTZ_UG_1Bkuun3FrvoAOQ6ACTx_LKm_k9ZOb4y629wYmTY3yYpf2jIconSb8ezyn_L7xVLDS1EgKvLCWO2p5eQ'),
+(47, 'm', 'dd', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjA2NzgxMTEsImV4cCI6MTYyMDY3ODcxMX0.kP0-kLnqyNfYZWOeMO6yB6yoIi-rHvof3g6MEp_GsF6yFzxvMRfug-jVaAJNpf32Ok9q9bybF3LZxfxyzrz0QA'),
+(51, 'modif', 'modif', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MjA2NzkzNzUsImV4cCI6MTYyMDY3OTk3NX0.1GpaBRsWon-4CUmTDO-LyZXz3DNzFepyejj2yiokuO9beoXQNguKVKU8E2VItGW-6H4s6_aEDMvFSDCoZFZVWA'),
+(52, 'login', 'login', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibG9naW4iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjIwNzQwOTgyLCJleHAiOjE2MjA3NDE1ODJ9.22-9IAYWad5-erv1Af1D3Mk2EPk97mXjmd-tTb2Dw5f3ntqp38gJAoQJMOKEEHJFdFwHV8jL8Dqmqi6sHK8K4w'),
+(53, 'login', 'login', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibG9naW4iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjIwNjc5ODcwLCJleHAiOjE2MjA2ODA0NzB9.DABZDP5khfYSYd-cr6iZZGAASdiWAa43wrPnjhyqPqZd8yCmUFR94j8Ht0bqhUOC323zyuEVeZjSPo3p5BhG7A'),
+(54, 'martintestId', 'aaa', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWFydGludGVzdElkIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYyMDc0Mjk1MywiZXhwIjoxNjIwNzQzNTUzfQ.q38ni58MJLzRdSrskfDm0O8TRBmVOnzaULwDwQAo03k1gyLKHCFZ0_r5rYxPzxoVP36oRjSqZqt1eAdHID-uzA'),
+(55, 'login', 'login', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibG9naW4iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjIwNjgzODExLCJleHAiOjE2MjA2ODQ0MTF9.3B43YpmXj2m36Gg6M-U2f8qQm5oI4RosY3OvkIkgBzs-KREXA1R1lzLJtPYqHogeaBg-NWtAjVJuLoThm78peA'),
+(56, 'login', 'login', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibG9naW4iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjIwNjgzODIxLCJleHAiOjE2MjA2ODQ0MjF9.WhlYbJ512gIJuOO4lICmDZd_xzGEVcP7PjJ2tIZWhU3HUiP_5Id5yfE1lk0yzfK2oUbighEQF8mpXHlDb9UnLQ'),
+(57, 'login', 'login', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibG9naW4iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjIwNzI0OTU2LCJleHAiOjE2MjA3MjU1NTZ9.ASmGFcuN-8J_4Pq8J8iPHoK8i1MKKdd1vGQKrweh-yOQPXqcsfCE2K4kvhta19asqrhKflG2SZ-1xBv2y1UAVw'),
+(58, 'martintestId', 'aaa', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoibWFydGludGVzdElkIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYyMDc0MTQ1MSwiZXhwIjoxNjIwNzQyMDUxfQ.xp8sVg5BA1KIukXEZclH-vNGCJynAp_6d9_TdVWQbVd9eoiK8_EAq3q5Jx2bZltYJuHbD3TAuzP6Qu4c_P2mDA');
 
 -- --------------------------------------------------------
 
@@ -72,8 +87,8 @@ CREATE TABLE `hibernate_sequence` (
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(41),
-(41);
+(63),
+(63);
 
 -- --------------------------------------------------------
 
@@ -129,9 +144,9 @@ INSERT INTO `historique_info` (`id_hist_info`, `dateheure`, `id_info`, `id_clien
 CREATE TABLE `information` (
                                `id_info` int(11) NOT NULL,
                                `luminosite` int(11) DEFAULT NULL,
-                               `niv_batt` int(11) DEFAULT NULL,
-                               `pression` int(11) DEFAULT NULL,
-                               `temperature` int(11) DEFAULT NULL,
+                               `proximite` int(11) DEFAULT NULL,
+                               `gravite` int(11) DEFAULT NULL,
+                               `acceleration` int(11) DEFAULT NULL,
                                `latitude` int(11) DEFAULT NULL,
                                `longitude` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -140,10 +155,14 @@ CREATE TABLE `information` (
 -- Déchargement des données de la table `information`
 --
 
-INSERT INTO `information` (`id_info`, `luminosite`, `niv_batt`, `pression`, `temperature`, `latitude`, `longitude`) VALUES
+INSERT INTO `information` (`id_info`, `luminosite`, `proximite`, `gravite`, `acceleration`, `latitude`, `longitude`) VALUES
 (1, 0, 78, 90, 23, 54, 31),
 (2, 54, 65, 100, 0, 89, 9),
-(3, 45, 0, 100, 19, 9, 43);
+(3, 45, 0, 100, 19, 9, 43),
+(59, 0, 78, 90, 23, 54, 31),
+(60, 0, 78, 90, 23, 54, 31),
+(61, 0, 78, 0, 23, 54, 31),
+(62, 0, 78, 0, 23, 0, 0);
 
 -- --------------------------------------------------------
 
